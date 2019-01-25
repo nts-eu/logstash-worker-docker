f33c997a55986762905d2a5cf4b47c56521eb993
Markus Rainer <rainer@nts.at>
2019-01-25 13:28:31 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tc qdisc add dev eth0 root handle 1: htb
+tc class add dev eth0 parent 1:1 classid 1:10 htb rate $BW_RATE ceil $BW_CEIL
+tc filter add dev eth0 parent 1:0 prio 1 protocol ip handle 10 fw flowid 1:10
+iptables -A OUTPUT -t mangle -p tcp --dport 443 -j MARK --set-mark 10
+
