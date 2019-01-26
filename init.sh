8c85e0fd2ed71bf6ab9981556405fcfdaf0dfb23
Markus Rainer <rainer@nts.at>
2019-01-26 08:19:52 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "setting max output BW for port 443 to $BW_RATE" > /var/tmp/init_sh.out
+
 tc qdisc add dev eth0 root handle 1: htb
 tc class add dev eth0 parent 1:1 classid 1:10 htb rate $BW_RATE ceil $BW_CEIL
 tc filter add dev eth0 parent 1:0 prio 1 protocol ip handle 10 fw flowid 1:10
